2bd5117a77f524b03075cae999111872a9e6975f
Kannathal Vairavan <kannamanickam@gmail.com>
2021-01-15 17:29:38 -0600

    Added setScore and renderScore function to the script file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var timeEl = document.querySelector("#time");
 var start = document.querySelector("#startButton");
 var questionDisplay=document.querySelector("#displayQuestion")
+var score = document.querySelector("#score")
 var questionindex =0;
 var score =0;
 start.addEventListener("click", function(){
@@ -55,28 +56,45 @@ var questionMaster=[
 function displayquiz(){
 var q = questionMaster[questionindex];
 
-
 }
 
 // quizscore function
+function setScore(){
+  //  Updates score to client storage
+  localStorage.setItem("score", score);
 
+}
+
+function renderScore(){
+  
+    // Get stored value from client storage, if it exists
+    var quizScore = localStorage.getItem("score");
+    // If stored value doesn't exist, set counter to 0
+    if (quizScore === null) {
+      score = 0;
+    } else {
+      // If a value is retrieved from client storage set the score to that value
+      score = quizScore;
+    }
+    //Render score  to page
+    score.textContent = score;
+ }
 // pass boolean variable result 
 // Each question will have 20 seconds
 var timeLeft = 100;
 // Penalty time 10
 var penalty = 10;
-
-function quizTimer(result) {
-    
+// result
+function quizTimer() {
   
     var timeInterval = setInterval(function() {
       // for each wrong answer take away 10 seconds
-      if (result === false) {timerEl.textContent = timeLeft-10}
+      if (result === false) {timeLeft = timeLeft-10}
       timerEl.textContent = timeLeft + " seconds remaining";
       timeLeft--;
   
       if (timeLeft === 0) {
-        timerEl.textContent = "";
+        timerEl.textContent = "Time's up!";
         
         clearInterval(timeInterval);
       }
